9c39cfbc868c7c692ba39c0b51e6ecf0d2046378
selectbutton-samples <d.ee.ro.be@gmail.com>
2015-12-09 21:49:55 -0500

    Initial commit

---
+++ Assets/Editor/Inspector_EnableSpriteShadow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomEditor(typeof(EnableSpriteShadow))]
+public class Inspector_EnableSpriteShadow : Editor {
+	public override void OnInspectorGUI()
+	{
+		EnableSpriteShadow targeted = (EnableSpriteShadow)target;
+		DrawDefaultInspector ();
+		if (GUI.changed)
+			targeted.SendMessage ("Reset");
+	}
+}
---
+++ Assets/Sprite Shadow/EnableSpriteShadow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EnableSpriteShadow : MonoBehaviour {
+
+	[SerializeField]
+	private static string		spriteMaterial = "Sprites-Default";
+	[SerializeField]
+	private Material 			defaultMaterial;
+	[SerializeField]
+	private Renderer 			targetRenderer;
+	[SerializeField]
+	private ShadowCastingMode 	shadowCastMode = ShadowCastingMode.TwoSided;
+	[SerializeField]
+	private bool 				receiveShadows = false;
+	
+	void Awake(){
+		if (targetRenderer || (targetRenderer = GetComponent<Renderer>()))
+		{
+			if (targetRenderer.sharedMaterial.name == spriteMaterial && defaultMaterial)
+				targetRenderer.sharedMaterial 	= defaultMaterial;
+			targetRenderer.shadowCastingMode 	= shadowCastMode;
+			targetRenderer.receiveShadows 		= receiveShadows;
+		}
+	}
+
+	void Reset(){
+		Awake ();
+	}
+
+	void Start () {
+		Awake ();
+	}
+}
